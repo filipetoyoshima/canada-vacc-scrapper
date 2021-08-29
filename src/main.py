d349cf72f7fa4f079d6d63c23e3d7cc006f8c4fe
Filipe Toyoshima <filipetoyoshima@gmail.com>
2021-08-29 11:09:27 -0300

    Adding support to search specific vaccine by args
    Receive a list of argv and search all received strings in the
    list of approved vaccines

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 from classes import scrapper
 from classes.web import Web
 from classes.scrapper import Scrapper
-from pprint import pprint
+from pprint import pp, pprint
+import re
+import sys
+
+
+def is_approved(vaccine:str, approved_list:list):
+    vaccine = vaccine.upper()
+    for approved in approved_list:
+        if re.search(f'{vaccine}', approved.upper()):
+            return True
+    return False
 
 
 if __name__ == '__main__':
@@ -11,3 +21,9 @@ if __name__ == '__main__':
     (approved_vaccines, disapproved_vaccines) = scrapper.get_vaccine_info(canada_page)
     pprint(approved_vaccines)
     pprint(disapproved_vaccines)
+    for searched_vacc in sys.argv[1:]:
+        if is_approved(searched_vacc, approved_vaccines):
+            pprint(f'{searched_vacc} found! :D')
+        else:
+            pprint(f'{searched_vacc} not found :(')
+
