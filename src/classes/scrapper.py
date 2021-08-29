dc60595c108ffe0e879a44f74e505750e02673ce
Filipe Toyoshima <filipetoyoshima@gmail.com>
2021-08-29 10:46:23 -0300

    Cleaning up vaccines results from pages
    The strings come with `\r\n` and a lot of unecessary spaces

--- src/classes/scrapper.py
+++ src/classes/scrapper.py
@@ -3,7 +3,7 @@ import re
 
 
 class Scrapper:
-    def get_vaccine_info(web_page):
+    def get_vaccine_info(self, web_page):
         """
         Input: HTML of 'COVID-19 vaccinated travellers entering Canada'
         web page.
@@ -18,5 +18,7 @@ class Scrapper:
         vaccine_lists = [
             [re.search('</span>(.*)</li>', str(vac), re.S).group(1) for vac in vac_list] for vac_list in [ul.find_all('li') for ul in uls]
         ]
+        vaccine_lists = [[re.sub('\r|\n', '', vac) for vac in vac_list] for vac_list in vaccine_lists]
+        vaccine_lists = [[re.sub('\s\s+', ' ', vac) for vac in vac_list] for vac_list in vaccine_lists]
         [approved_vaccs, disapproved_vaccs] = vaccine_lists
         return (approved_vaccs, disapproved_vaccs)
